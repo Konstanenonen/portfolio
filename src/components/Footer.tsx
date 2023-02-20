b6780dd904f57f6f6f5dd01fb6b817e28dae28f7
Konsta Nenonen <konstanenonen@protonmail.com>
2023-02-20 19:39:00 +0200

    Added more content to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { AiFillMail } from "react-icons/ai";
 import Styles from "./Footer.module.css";
 
 function Footer(): JSX.Element {
   return (
     <footer className={Styles.footer}>
-      <p className={Styles.title}>Contact me</p>
-      <p className={Styles.normalText}>
-        Please get in touch if you think our work could be mutually benefitical!
-      </p>
+      <div className={Styles.section1}>
+        <p className={Styles.title}>Contact me</p>
+        <p className={Styles.normalText}>
+          Please get in touch if you think our work could be mutually
+          benefitical!
+        </p>
+        <p className={Styles.normalText}>
+          <a href="konstanenonen@protonmail.com">
+            <AiFillMail /> konstanenonen@protonmail.com
+          </a>
+        </p>
+      </div>
+      <div className={Styles.section2}></div>
     </footer>
   );
 }
